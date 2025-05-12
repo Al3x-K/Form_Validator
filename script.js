9ef8520a66ec4ef26791d470857a15d01de6d204
Al3x-K <ola.kail1901@gmail.com>
2025-05-12 23:25:06 +0100

    ✨ Added functions to validate different inputs, added a popup display when the form is sent

--- script.js
+++ script.js
@@ -1,36 +1,114 @@
-const username = document.querySelector("#username");
-const password = document.querySelector("#password");
-const password2 = document.querySelector("#password2");
-const email = document.querySelector("#email");
+const username = document.querySelector('#username');
+const password = document.querySelector('#password');
+const password2 = document.querySelector('#password2');
+const email = document.querySelector('#email');
 
-const sendBtn = document.querySelector(".send");
-const clearBtn = document.querySelector(".clear");
-const popup = document.querySelector(".popup");
+const sendBtn = document.querySelector('.send');
+const clearBtn = document.querySelector('.clear');
+const popup = document.querySelector('.popup')
 
-
-const CheckForm = input => 
+const showError = (input, msg) => 
 {
-    if (input.value === '') 
+    const formBox = input.parentElement;    // Get the parent element of the input field
+    const errorMsg = formBox.querySelector('.error-text');
+
+    formBox.classList.add('error'); // Add the error class to the parent element
+    errorMsg.textContent = msg;
+}
+
+const clearError = (input) => 
+{
+    const formBox = input.parentElement;
+    formBox.classList.remove('error'); // Remove the error class from the parent element
+}
+
+const checkForm = input => 
+{
+    input.forEach(el =>   // Loop through each input field  
     {
-        console.log("Please fill in all fields");
+        if (el.value === '') 
+        {
+            showError(el, el.placeholder)   // Show error if the input is empty
+        } 
+        else 
+        {
+            clearError(el)
+        };
+    });
+};
+
+const checkLength = (input, min) => 
+{
+    if (input.value.length < min) 
+    {
+        showError(input, `${input.previousElementSibling.innerText.slice(0, -1)} should contain at least ${min} characters.`)// Check if the input length is less than the minimum
     }
 }
 
-sendBtn.addEventListener("click", (e) =>
+const checkPassword = (password, password2) => 
+{
+    if (password.value !== password2.value) 
+    {
+        showError(password2, 'Passwords do not match')
+    }
+}
+
+const checkEmail = email => 
+{
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // Regular expression for email validation
+
+    if (re.test(email.value)) 
+    {
+        clearError(email)
+    } 
+    else 
+{
+        showError(email, 'Email is not valid')
+    }
+}
+
+const checkErrors = () => 
+{
+
+    const allInputs = document.querySelectorAll('.form-box');
+    let errorCount = 0;
+
+    allInputs.forEach(el => 
+    {
+        if (el.classList.contains('error')) 
+        {
+            errorCount++
+        }
+    })
+
+    if (errorCount === 0) 
+    {
+        popup.classList.add('show-popup')
+    }
+}
+
+
+sendBtn.addEventListener('click', e => 
 {
     e.preventDefault();
 
-    CheckForm(username);
+    checkForm([username, password, password2, email]) // Check if all inputs are filled
+    checkLength(username, 3); // Check username length
+    checkLength(password, 8); // Check password length
+    checkPassword(password, password2) // Check if passwords match
+    checkEmail(email);// Check if email is valid
+    checkErrors()// Check if there are any errors
+    // If there are no errors, show the popup
 
-});
+})
 
-clearBtn.addEventListener("click", (e) => 
+clearBtn.addEventListener('click', e => 
 {
     e.preventDefault();
 
     [username, password, password2, email].forEach(el => 
     {
-        el.value = "";
+        el.value = ''
+        clearError(el)
     })
-});
-
+})
